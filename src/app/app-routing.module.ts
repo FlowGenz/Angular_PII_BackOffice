8b95af955c00975a43be46cb0b871df7f39b79ac
FlowGenZ <47348303+FlowGenz@users.noreply.github.com>
2019-11-18 23:38:04 +0100

    08 -- Changement navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import { ConnectionScreenComponent } from './connection-screen/connection-screen
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { DressListComponent } from './dress-list/dress-list.component';
 import { OrderListComponent } from './order-list/order-list.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [
   {path: '', component: ConnectionScreenComponent},
   {path: 'customerList', component: CustomerListComponent},
   {path: 'dressList', component: DressListComponent},
-  {path: 'orderList', component: OrderListComponent}
+  {path: 'orderList', component: OrderListComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
