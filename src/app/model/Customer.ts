2d99a5f6bb3c4bee32e0d883857c64dbf66429bb
FlowGenZ <47348303+FlowGenz@users.noreply.github.com>
2019-11-18 10:12:45 +0100

    02 -- Material and interfaces

---
+++ src/app/model/Customer.ts
@@ -0,0 +1,11 @@
+export interface Customer {
+    id: number;
+    username: string;
+    password: string;
+    prenom: string;
+    nom: string;
+    adresseMail: string;
+    numeroTelephone: string;
+    adresse: string;
+    pointsFidelite: number;
+}
