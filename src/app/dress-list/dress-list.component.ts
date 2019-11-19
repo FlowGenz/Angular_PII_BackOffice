d38ad4bebadd0b2faf62031ae4a3db8c24fbffc1
FlowGenZ <47348303+FlowGenz@users.noreply.github.com>
2019-11-19 17:46:22 +0100

    10.2 -- pagination suite

--- src/app/dress-list/dress-list.component.ts
+++ src/app/dress-list/dress-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { Dress } from '../model/Dress';
 import { DressService } from '../dress.service';
 import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-dress-list',
@@ -12,13 +13,14 @@ export class DressListComponent implements OnInit {
 
   displayedColumns: string[] = ['id', 'nom', 'description', 'disponibilite', 'dateDebutDisponibilite',
                                 'dateFinDisponibilite', 'partenaireId'];
-  dataSource: Array<Dress>;
+  dataSource: MatTableDataSource<Dress>;
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   constructor(private service: DressService) { }
 
   ngOnInit() {
-    this.dataSource = this.service.getAll();
+    this.dataSource = new MatTableDataSource(this.service.getAll());
+    this.dataSource.paginator = this.paginator;
   }
 }
