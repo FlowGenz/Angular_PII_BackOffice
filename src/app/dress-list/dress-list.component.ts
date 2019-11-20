91110a78effda53971906fcf5d8be3971bcbec4a
FlowGenZ <47348303+FlowGenz@users.noreply.github.com>
2019-11-20 11:05:46 +0100

    10.3 -- pagination traduction

--- src/app/dress-list/dress-list.component.ts
+++ src/app/dress-list/dress-list.component.ts
@@ -3,6 +3,9 @@ import { Dress } from '../model/Dress';
 import { DressService } from '../dress.service';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
+import {SelectionModel} from '@angular/cdk/collections';
+
+const LABEL_PAGINATOR: string = "Nombre de robes par page :";
 
 @Component({
   selector: 'app-dress-list',
@@ -22,5 +25,6 @@ export class DressListComponent implements OnInit {
   ngOnInit() {
     this.dataSource = new MatTableDataSource(this.service.getAll());
     this.dataSource.paginator = this.paginator;
+    this.dataSource.paginator._intl.itemsPerPageLabel = LABEL_PAGINATOR;
   }
 }
